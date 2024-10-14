a083cb1f4f7f984b74e54719fc3d7fc794d16c5e
Vishal Omprasad <vishal.omprasad@syncfusion.com>
2024-10-14 18:20:37 +0530

    Issue reproducible sample added

---
+++ InkCanvasDemo/InkCanvasDemo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InkCanvasDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void inkCanvasButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                InkCanvas inkCanvas = new InkCanvas();
+            }
+        }
+    }
+}
